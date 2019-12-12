04488593f26c7e47bca72b5a0b4c47da196ff3f0
io <io@DESKTOP-DP9DE2D>
2019-12-12 20:34:15 +0100

    ejercicios124

---
+++ src/boletinfiguras/BoletinFiguras.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletinfiguras;
+
+/**
+ *
+ * @author io
+ */
+public class BoletinFiguras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Figuras obx = new Figuras();
+        obx.Triangulo();
+        Figuras obx1 = new Figuras();
+       obx1.Piramide();
+    }
+    
+}
+    
+    
+
